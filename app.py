a24b0b01648a922692945abe0e895a9f405f28f6
v1ncentq <focusep12@gmail.com>
2025-06-04 18:09:17 +0300

    refactoring for google cloud

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-import re
+from google.cloud import firestore
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-
-class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    custom_title = db.Column(db.String(100), nullable=False)
-    youtube_url = db.Column(db.String(200), nullable=False)
-
-
+db = firestore.Client()
 
 @app.route('/')
 def index():
@@ -23,22 +13,25 @@ def add_video():
     custom_title = request.form['custom_title']
     youtube_url = request.form['youtube_url']
     
-    # Простая валидация URL
-    if 'youtube.com' not in youtube_url and 'youtu.be' not in youtube_url:
+    # Валидация URL
+    if not ('youtube.com' in youtube_url or 'youtu.be' in youtube_url):
         return "Неверная YouTube ссылка!", 400
     
-    new_video = Video(custom_title=custom_title, youtube_url=youtube_url)
-    db.session.add(new_video)
-    db.session.commit()
+    # Добавление в Firestore
+    doc_ref = db.collection('videos').document()
+    doc_ref.set({
+        'custom_title': custom_title,
+        'youtube_url': youtube_url
+    })
     
     return redirect(url_for('show_videos'))
 
 @app.route('/videos')
 def show_videos():
-    videos = Video.query.all()
+    videos = []
+    docs = db.collection('videos').stream()
+    for doc in docs:
+        video = doc.to_dict()
+        video['id'] = doc.id
+        videos.append(video)
     return render_template('videos.html', videos=videos)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
